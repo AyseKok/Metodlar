f1764d4a597c60e373cb7a4e58b0d2d9fb8f8647
Ayşe Kök <ayse2696.ak@outlook.com>
2021-01-27 18:29:45 +0300

    23 ocak ders sonu

---
+++ OOP1/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class Product
+    {
+        public int Id { get; set; }
+        public int CatehoryId { get; set; }
+        public string ProductName { get; set; }
+        public double UnitPrice { get; set; } //birim fiyat
+        public int UnitsInStock { get; set; } //stok adedi
+
+        
+    }
+}
